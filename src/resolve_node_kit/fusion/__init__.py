a38b2bf66517976d1da5fac259587fdf90a4c861
46slv <254147489+46slv@users.noreply.github.com>
2026-09-05 20:37:53 +0900

    feat: export recursive GroupOperator tidy command

--- src/resolve_node_kit/fusion/__init__.py
+++ src/resolve_node_kit/fusion/__init__.py
@@ -1,3 +1,10 @@
+from .recursive_groups import GroupTidyResult, tidy_groups_comp
 from .tidy import FusionHostError, TidyResult, tidy_comp
 
-__all__ = ["FusionHostError", "TidyResult", "tidy_comp"]
+__all__ = [
+    "FusionHostError",
+    "GroupTidyResult",
+    "TidyResult",
+    "tidy_comp",
+    "tidy_groups_comp",
+]
